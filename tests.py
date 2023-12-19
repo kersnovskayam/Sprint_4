c54ae3866af66cfb5a87ae761356f32c38d8206c
Mariya Kersnovskaya <mariakersnovskaya@yandex.ru>
2023-12-19 17:26:11 +0300

    tests_books Исправленная работа с учетом замечаний

--- tests.py
+++ tests.py
@@ -9,7 +9,7 @@ def books_collector():
 @pytest.mark.parametrize("book_name", ["Книга 1"])
 def test_add_new_book(books_collector, book_name):
     books_collector.add_new_book(book_name)
-    assert book_name in books_collector.books_genre
+    assert book_name in books_collector.get_books_genre()
 
 # Тест установки жанра книге
 @pytest.mark.parametrize("new_genre, result",
@@ -18,7 +18,7 @@ def test_add_genre(books_collector, new_genre, result):
     book_name = "Книга"
     books_collector.add_new_book(book_name)
     books_collector.set_book_genre(book_name, new_genre)
-    is_genre_added = books_collector.books_genre[book_name] == new_genre
+    is_genre_added = books_collector.get_book_genre(book_name) == new_genre
     assert (is_genre_added and result) or (not is_genre_added and not result)
 
 # Тест получения жанра книги
@@ -29,26 +29,39 @@ def test_get_book_genre(books_collector, new_genre, expected_result):
     books_collector.set_book_genre(book_name, new_genre)
     assert books_collector.get_book_genre(book_name) == expected_result
 
-# Тест проверки установки жанра книги
-def test_set_book_genre(books_collector):
-    books_collector.add_new_book("Книга 1")
-    books_collector.set_book_genre("Книга 1", "Фантастика")
-    genre = books_collector.get_book_genre("Книга 1")
-    assert genre == "Фантастика"
-
 # Тест получения списка книг конкретного жанра
 def test_get_books_by_genre(books_collector):
-    books_collector.books_genre = {"Книга 1":"Фантастика", "Книга 2":"Фантастика", "Книга 3":"Фантастика", "Книга 4":"Ужасы"}
+    books_collector.add_new_book("Книга 1")
+    books_collector.set_book_genre("Книга 1", "Фантастика")
+    books_collector.add_new_book("Книга 2")
+    books_collector.set_book_genre("Книга 2", "Фантастика")
+    books_collector.add_new_book("Книга 3")
+    books_collector.set_book_genre("Книга 3", "Фантастика")
+    books_collector.add_new_book("Книга 4")
+    books_collector.set_book_genre("Книга 4", "Ужасы")
+
     genre_books = books_collector.get_books_with_specific_genre("Фантастика")
+
     assert "Книга 1" in genre_books
     assert "Книга 2" in genre_books
     assert "Книга 3" in genre_books
     assert len(genre_books) == 3
 
 # Тест проверки отсутствия книг с возрастным рейтингом в списке книг для детей
-def test_get_children_books(books_collector):
-    books_collector.books_genre = {"Книга 1":"Детективы", "Книга 2":"Ужасы", "Книга 3":"Фантастика", "Книга 4":"Комедии", "Книга 5": "Мультфильмы"}
+def get_children_books(books_collector):
+    books_collector.add_new_book("Книга 1")
+    books_collector.get_book_genre("Книга 1", "Детективы")
+    books_collector.add_new_book("Книга 2")
+    books_collector.get_book_genre("Книга 2", "Ужасы")
+    books_collector.add_new_book("Книга 3")
+    books_collector.get_book_genre("Книга 3", "Фантастика")
+    books_collector.add_new_book("Книга 4")
+    books_collector.get_book_genre("Книга 4", "Комедии")
+    books_collector.add_new_book("Книга 5")
+    books_collector.get_book_genre("Книга 5", "Мультфильмы")
+
     children_books = books_collector.get_books_for_children()
+
     assert "Книга 1" not in children_books
     assert "Книга 2" not in children_books
     assert "Книга 3" in children_books
@@ -76,12 +89,14 @@ def test_delete_book_from_favorites(books_collector):
     assert "Книга 1" in favorites
     assert "Книга 2" not in favorites
 
-# Тест проверки получения списка избранных книг
-def test_get_list_of_favorites_books(books_collector):
-    books_collector.add_new_book("Книга 1")
-    books_collector.add_new_book("Книга 2")
+# Тест проверки добавления невалидного названия книги
+@pytest.mark.parametrize("invalid_book_name", ['', "a" * 41])
+def test_add_invalid_book_name(books_collector, invalid_book_name):
+    books_collector.add_new_book(invalid_book_name)
+    assert invalid_book_name not in books_collector.get_books_genre()
+
+# Тест проверки добавления несуществующей книги в избранное
+def test_add_not_existed_book_in_favorites (books_collector):
     books_collector.add_book_in_favorites("Книга 1")
-    books_collector.add_book_in_favorites("Книга 2")
     favorites = books_collector.get_list_of_favorites_books()
-    assert "Книга 1" in favorites
-    assert "Книга 2" in favorites
+    assert ("Книга 1") not in favorites
